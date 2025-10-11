aae41d8c32a5626263a02ce50c343ab22698ed99
bashytech007 <bashiraremu474@gmail.com>
2025-10-11 15:01:01 +0100

    expo-54-with-nativewind-expo-router

---
+++ src/app/_layout.tsx
@@ -0,0 +1,15 @@
+import {Stack} from 'expo-router';
+import {DarkTheme, ThemeProvider} from '@react-navigation/native'
+import "../../global.css"
+
+export default function RootLayout(){
+    return (
+        <ThemeProvider value={DarkTheme}>
+        <Stack>
+            <Stack.Screen name="index" options={{title:"Index",headerLargeTitle:true}}>
+
+            </Stack.Screen>
+        </Stack>
+    </ThemeProvider>
+    )
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,8 @@
+import {View,Text} from 'react-native'
+export default function Home(){
+return (
+    <View className='flex-1 justify-center items-center bg-red-500'>
+        <Text className='text-2xl font-bold'>Expo 54 using native wind</Text>
+    </View>
+)
+}
